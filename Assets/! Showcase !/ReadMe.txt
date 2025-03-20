0f4a0fcb7c74f42a0dfba569110ad9cceffccecb
Michał Zawitaj <michalzawitaj@gmail.com>
2025-03-20 23:59:43 +0100

    TG-1 #in-progress Wstęp do tworzenia świata gry
    Porządek w folderach, prefabach, materiałach. Sceny testowe dla modeli.
    Pierwszy gotowy segment lasu do powielania

---
+++ Assets/! Showcase !/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿1.	Forrest_1 to moim zdaniem ciekawy koncept tworzenia większej mapki, 
+	jest to kilka obiektów połączonych w wmiarę naturalnie wyglądający fragment lasu.
+	Jeżeli w przyszłości powstanie np. Forrest_2, Forrest_3 (bloki o podobnych wymiarach), 
+	będzie nam bardzo łatwo budować z tego większą mape z uniknięcem zbędnej powtarzalności.
+2.	
---
+++ Assets/Michał/Scripts/Showcase_Camera.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using TMPro;
+
+public class Showcase_Camera : MonoBehaviour
+{
+    public float baseSpeed = 10.0f;
+    public float speedMultiplier = 1.0f;
+    public float minSpeedMultiplier = 0.01f;
+    public float maxSpeedMultiplier = 2.0f;
+    public float scrollSensitivity = 0.1f;
+    public TMP_Text speedMultiplierText; 
+
+    void Start()
+    {
+        UpdateSpeedMultiplierText();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleSpeedChange();
+    }
+
+    void HandleMovement()
+    {
+        float speed = baseSpeed * speedMultiplier;
+        Vector3 direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += transform.right;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            direction -= transform.up;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            direction += transform.up;
+        }
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        float rotationX = Input.GetAxis("Mouse X");
+        float rotationY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up, rotationX, Space.World);
+        transform.Rotate(Vector3.left, rotationY, Space.Self);
+    }
+
+    void HandleSpeedChange()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0.0f)
+        {
+            speedMultiplier += scroll * scrollSensitivity;
+            speedMultiplier = Mathf.Clamp(speedMultiplier, minSpeedMultiplier, maxSpeedMultiplier);
+            UpdateSpeedMultiplierText();
+        }
+    }
+
+    void UpdateSpeedMultiplierText()
+    {
+        if (speedMultiplierText != null)
+        {
+            speedMultiplierText.text = $"Speed Multiplier: {speedMultiplier:F2}";
+        }
+    }
+}
+
